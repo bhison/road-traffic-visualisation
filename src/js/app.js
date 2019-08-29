e40151f6941580c04b507315b9ed82f682a850a4
bhison <catbat04@gmail.com>
2019-08-29 23:31:16 +0100

    basic display of points

--- src/js/app.js
+++ src/js/app.js
@@ -5,31 +5,23 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYmhpc29uIiwiYSI6ImNqcDc5a2xlaTEwNmwza28zMDFvY
 
 var map = window.map = new mapboxgl.Map({
   container: 'map',
-  zoom: 12,
-  center: [-3.5485669, 50.724423],
+  zoom: 8,
+  center: [-3.819440, 50.724423],
   style: 'mapbox://styles/bhison/cjzx4fu180ehk1crs3kl0eis1'
 });
 
 map.on('load', function () {
   GenerateDataSource(processedData => {
-    // map.addSource('traffic-data', {
-    //   'type': 'geojson',
-    //   'data': processedData
-    // });
+    map.addSource('traffic-data', {
+      'type': 'geojson',
+      'data': processedData
+    });
 
-    // map.addLayer({
-    //   "id": "points",
-    //   "type": "symbol",
-    //   "source": 'traffic-data',
-    //   "layout": {
-    //     "icon-image": "{icon}-15",
-    //     "text-field": "{title}",
-    //     "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-    //     "text-offset": [0, 0.6],
-    //     "text-anchor": "top"
-    //   }
-    // });
-    console.log(processedData);
+    map.addLayer({
+      "id": "points",
+      "type": "circle",
+      "source": 'traffic-data'
+    });
   });
 
 
@@ -42,7 +34,7 @@ function GenerateDataSource(callback) {
     "features": []
   };
   sampleData.data.forEach(element => {
-    var existingObjectsWithCountPointId = processedData.features.filter(i =>
+    let existingObjectsWithCountPointId = processedData.features.filter(i =>
       i.properties.pointId == element.count_point_id
     );
     if (existingObjectsWithCountPointId.length <= 0) {
@@ -73,7 +65,7 @@ function GenerateDataSource(callback) {
         }
       })
     } else {
-      //add the values from the alternative direction to the totals
+      //consolidate the counts from the all directions
       const match = existingObjectsWithCountPointId[0];
       match.properties.counts.pushbikes += Number(element.pedal_cycles);
       match.properties.counts.motorbikes += Number(element.two_wheeled_motor_vehicles);

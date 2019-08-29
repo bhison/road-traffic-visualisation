44bf211258fa7ef6e4124fdec86bb38188075438
bhison <catbat04@gmail.com>
2019-08-29 21:04:54 +0100

    added basic controls placeholder

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,23 @@
-console.log("Hello world");
+import "../css/main.css";
+import { data } from '../data/data.json';
+
+/*
+const data =
+  response.data.map(d => {
+    latitude: d.latitude;
+    longitude: d.longitude;
+    roadType: d.road_type;
+    roadName: d.road_name;
+    startJunction: d.start_junction_road_name;
+    endJunction: d.end_junction_road_name;
+    counts: {
+      pushbikes: d.pedal_cycles;
+      motorbikes: d.two_wheeled_motor_vehicles;
+      cars: d.cars_and_taxis;
+      buses: d.buses_and_coaches;
+      lgvs: d.lgvs;
+      hgvs: d.all_hgvs;
+      alllMotorVehicles: d.all_motor_vehicles;
+    }
+  });
+*/
